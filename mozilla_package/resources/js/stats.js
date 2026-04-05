41f314b30a8b147842e96c25ea973678b51fd416
vallanx-team <laurent.schueller@googlemail.com>
2026-04-05 17:00:07 +0200

    presentation of time value

--- mozilla_package/resources/js/stats.js
+++ mozilla_package/resources/js/stats.js
@@ -12,6 +12,8 @@ export function formatMb(mb) {
 
 export function formatMs(ms) {
   if (ms < 1000) return `${ms}ms`;
+  if (ms > 60000) return `${(ms / 60000).toFixed(1)}m`;
+  if (ms > 3600000) return `${(ms / 3600000).toFixed(1)}h`;
   return `${(ms / 1000).toFixed(1)}s`;
 }
 
